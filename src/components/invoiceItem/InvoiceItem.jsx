22423e4e859f17767da9db7aeff6ede02d210fe1
leotricotti <tricottileo@gmail.com>
2023-06-06 17:25:20 -0300

    commit

--- src/components/invoiceItem/InvoiceItem.jsx
+++ src/components/invoiceItem/InvoiceItem.jsx
@@ -1,34 +1,59 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFileLines,
+  faFolderOpen,
+  faPrint,
+} from "@fortawesome/free-solid-svg-icons";
 import styles from "./invoiceItem.module.css";
 
 function InvoiceItem({ invoices }) {
   console.log(invoices);
-  return invoices.invoices.map((invoice) => (
-    <div key={invoice.number}>
-      <h3>Factura N° {invoice.number}</h3>
-      <table>
-        <tbody>
-          <tr>
-            <td>
-              <strong>Cliente:</strong>
-            </td>
-            <td>{invoice.costumer}</td>
-          </tr>
-          <tr>
-            <td>
-              <strong>Fecha:</strong>
-            </td>
-            <td>{invoice.date}</td>
-          </tr>
-          <tr>
-            <td>
-              <strong>Total:</strong>
-            </td>
-            <td>$ {invoice.total}</td>
-          </tr>
-        </tbody>
-      </table>
+  return (
+    <div className={styles.invoiceContainer}>
+      {invoices.invoices.map((invoice) => (
+        <div key={invoice.number} className={styles.invoiceInner}>
+          <div className={styles.iconContainer}>
+            <FontAwesomeIcon icon={faFileLines} className={styles.icon} />
+          </div>
+          <h4 className={styles.title}>
+            Factura # <br /> {invoice.number}
+          </h4>
+          <table>
+            <tbody className={styles.body}>
+              <tr className={styles.item}>
+                <td>
+                  <p>Cliente</p>
+                </td>
+                <td>
+                  <strong>{invoice.customer}</strong>
+                </td>
+              </tr>
+              <tr className={styles.item}>
+                <td>
+                  <strong>Fecha:</strong>
+                </td>
+                <td>{invoice.date}</td>
+              </tr>
+              <tr className={styles.item}>
+                <td>
+                  <strong>Total:</strong>
+                </td>
+                <td>$ {invoice.total}</td>
+              </tr>
+            </tbody>
+          </table>
+          <div className={styles.options}>
+            <button>
+              <FontAwesomeIcon icon={faFolderOpen} />
+            </button>
+            <button>
+              <FontAwesomeIcon icon={faPrint} />
+            </button>
+          </div>
+        </div>
+      ))}
     </div>
-  ));
+  );
 }
 
 export default InvoiceItem;
