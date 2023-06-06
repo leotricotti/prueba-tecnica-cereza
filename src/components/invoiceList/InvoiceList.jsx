472d4b1d20e7688e2f5615f5b2c8defa42d4abeb
Leonardo Tricotti <tricottileo@gmail.com>
2023-06-06 12:36:07 -0300

    commit

---
+++ src/components/invoiceList/InvoiceList.jsx
@@ -0,0 +1,16 @@
+import styles from "./InvoiceList.module.css";
+
+function InvoiceList({ title, invoices }) {
+  return (
+    <section className={styles.invoiceList}>
+      <h2 className={styles.title}>{title}</h2>
+      <ul className={styles.listContainer}>
+        {invoices.map((invoice, index) => (
+          <InvoiceItem key={index} invoice={invoice} />
+        ))}
+      </ul>
+    </section>
+  );
+}
+
+export default InvoiceList;
