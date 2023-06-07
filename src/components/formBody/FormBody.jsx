17670b04e428da708aa437e1ff4a8b96722e0b5a
Leonardo Tricotti <tricottileo@gmail.com>
2023-06-07 18:43:25 -0300

    commit

---
+++ src/components/formBody/FormBody.jsx
@@ -0,0 +1,44 @@
+import styles from "./formBody.module.css";
+
+function FormBody({ invoiceDetail, handleRowChange }) {
+  return (
+    <form>
+      {invoiceDetail.map((row, index) => (
+        <div key={index} className={styles.invoiceRow}>
+          <input
+            type="number"
+            value={row.quantity}
+            onChange={(e) => handleRowChange(index, "quantity", e.target.value)}
+            required
+            className={styles.quantity}
+          />
+          <input
+            type="text"
+            value={row.description}
+            onChange={(e) =>
+              handleRowChange(index, "description", e.target.value)
+            }
+            required
+            className={styles.description}
+          />
+          <input
+            type="number"
+            value={row.price}
+            onChange={(e) => handleRowChange(index, "price", e.target.value)}
+            required
+            className={styles.price}
+          />
+          <input
+            type="number"
+            value={row.total}
+            onChange={(e) => handleRowChange(index, "total", e.target.value)}
+            required
+            className={styles.total}
+          />
+        </div>
+      ))}
+    </form>
+  );
+}
+
+export default FormBody;
