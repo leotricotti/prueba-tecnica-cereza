562e181d4cadc1c4a79acb25f16f83e630f55f74
Leonardo Tricotti <tricottileo@gmail.com>
2023-06-08 12:11:01 -0300

    commit

--- src/components/formBody/FormBody.jsx
+++ src/components/formBody/FormBody.jsx
@@ -3,16 +3,17 @@ import styles from "./formBody.module.css";
 
 function FormBody({ invoiceDetail, handleRowChange }) {
   return (
-    <section className={styles.formBodyContainer}>
-      <FormRow
-        invoiceDetail={invoiceDetail}
-        handleRowChange={handleRowChange}
-      />
-      <FormRow
-        invoiceDetail={invoiceDetail}
-        handleRowChange={handleRowChange}
-      />
+    <section className={styles.formBody}>
+      {invoiceDetail.map((index) => (
+        <FormRow
+          key={index}
+          invoiceDetail={invoiceDetail}
+          handleRowChange={handleRowChange}
+          index={index}
+        />
+      ))}
     </section>
   );
 }
+
 export default FormBody;
