9d372a81035ecef76f754fa98251c861002a2aaa
Leonardo Tricotti <tricottileo@gmail.com>
2023-06-09 15:20:56 -0300

    commit

---
+++ src/context/dataContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useEffect, useState } from "react";
+
+const DataContext = createContext();
+
+const DataProvider = ({ children }) => {
+  const [data, setData] = useState([]);
+
+  console.log(data);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products");
+        const jsonData = await response.json();
+        setData(jsonData);
+      } catch (error) {
+        console.error("Error while fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  return <DataContext.Provider value={data}>{children}</DataContext.Provider>;
+};
+
+export { DataContext, DataProvider };
