2a5cc5c31e8f79bd4fbdfc2c170d5a422e258b91
Leonardo Tricotti <tricottileo@gmail.com>
2023-06-11 16:40:09 -0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from "react";
+import { Route, Routes } from "react-router-dom";
 import InvoiceList from "./pages/invoiceList/InvoiceList";
 import Spinner from "./components/spinner/Spinner";
 
@@ -11,7 +12,13 @@ function App() {
     }, 1500);
   }, [isLoading]);
 
-  return isLoading ? <Spinner /> : <InvoiceList />;
+  return isLoading ? (
+    <Spinner />
+  ) : (
+    <Routes>
+      <Route path="/" element={<InvoiceList />} />
+    </Routes>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { DataProvider } from "./context/dataContext";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <DataProvider>
-      <App />
-    </DataProvider>
+    <BrowserRouter>
+      <DataProvider>
+        <App />
+      </DataProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
