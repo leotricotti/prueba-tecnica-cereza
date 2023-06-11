d1f6406971438b6ee3bf4e750bdd385ee03ca027
Leonardo Tricotti <tricottileo@gmail.com>
2023-06-11 18:36:50 -0300

    commit

--- src/components/invoiceListHeader/invoiceListHeader.jsx
+++ src/components/invoiceListHeader/invoiceListHeader.jsx
@@ -1,20 +1,29 @@
-import Navbar from "../navbar/Navbar";
-import styles from "./invoicelistHeader.module.css";
+import SearchBar from "../searchBar/SearchBar";
+import Button from "../button/Button";
+import styles from "./navbar.module.css";
 
-import React from "react";
-
-function Header({ data, link, setShowInvoice, onClick, setIsLoading }) {
+function InvoiceListHeader({
+  data,
+  setShowInvoice,
+  link,
+  setIsLoading,
+  onClick,
+}) {
   return (
-    <header className={styles.headerContainer}>
-      <Navbar
+    <nav className={styles.navContainer}>
+      <SearchBar
         data={data}
-        setShowInvoice={setShowInvoice}
-        onClick={onClick}
-        link={link}
+        setShowInvoice={() => setShowInvoice(true)}
         setIsLoading={setIsLoading}
       />
-    </header>
+      <Button
+        text="Crear factura"
+        onClick={onClick}
+        styles={styles.button}
+        link={link}
+      />
+    </nav>
   );
 }
 
-export default Header;
+export default InvoiceListHeader;
